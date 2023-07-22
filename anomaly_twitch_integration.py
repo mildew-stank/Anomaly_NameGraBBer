2ce62a80298293973a383cb9af1a319828d3fbad
mildew-stank <139667119+mildew-stank@users.noreply.github.com>
2023-07-21 21:38:26 -0400

    refactoring
    separated namegrabber from anomaly integration so that it can be used with other mods.
    added error handling.

---
+++ anomaly_twitch_integration.py
@@ -0,0 +1,77 @@
+# TODO: check gamma compatibility
+
+import xml.etree.ElementTree as xml_element_tree
+import namegrabber
+
+
+def add_twitch_names(names):
+    entry_amount = 0
+
+    try:
+        tree = xml_element_tree.parse("reference_generate_snames.xml")
+        root = tree.getroot()
+    except IOError as error:
+        print("File reference_generate_snames.xml not found: ")
+        raise SystemExit(error)
+
+    for name in names:
+        entry_amount += 1  # this occurs here because update_name_amount needs the one-indexed number
+        # create elements
+        new_string_element = xml_element_tree.SubElement(root, "string", id="lname_stalker_" + str(entry_amount - 1))
+        new_text_element = xml_element_tree.SubElement(new_string_element, "text")
+        # format elements
+        new_string_element.text = "\n\t"
+        new_string_element.tail = "\n"
+        new_text_element.text = name
+        new_text_element.tail = "\n"
+
+    tree.write("gamedata/configs/text/eng/st_generate_snames.xml")
+    update_name_amount(entry_amount)
+
+    return entry_amount
+
+
+def update_name_amount(entry_amount):
+    lines = []
+
+    try:
+        with open("gamedata/configs/system.ltx", "r") as in_file:
+            lines = in_file.readlines()
+            lines[463] = ("last_name_cnt                            = " + str(entry_amount) + "\n")
+    except IOError as error:
+        print("File gamedata/configs/system.ltx not found: ")
+        raise SystemExit(error)
+
+    with open("gamedata/configs/system.ltx", "w") as out_file:
+        for line in lines:
+            out_file.write(line)
+
+
+def main():
+    print("Authorization link:")
+    print("https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=w70kisl5o6k8wsbrh8w41938vw9nk6&redirect_uri=http://localhost&scope=moderator%3Aread%3Achatters")
+    print("")
+    print("After being redirected to an invalid page you can retrieve your user access token from the url bar")
+    print("")
+    print("Example:")
+    print("http://localhost/#access_token= <USER_ACCESS_TOKEN> &scope=moderator%3Aread%3Achatters&token_type=bearer")
+    print("")
+    access_token = input("Input user access token: ")
+    print("")
+
+    name_grabber = namegrabber.NameGraBBer("w70kisl5o6k8wsbrh8w41938vw9nk6", access_token)
+    user_id = name_grabber.get_user_id_from_access_token()
+    names = name_grabber.get_chat_names_from_user_id(user_id)
+    names_added = add_twitch_names(names)
+
+    if names_added >= 1000:
+        print("Current name limit exceeded. Only the first 1000 were added, in alphabetical order")
+    else:
+        print("Names added: " + str(names_added))
+
+    print("Start a new game to populate the world with updated names")
+    input()
+
+
+if __name__ == "__main__":
+    main()
---
+++ namegrabber.py
@@ -0,0 +1,54 @@
+# TODO: remove 1,000 chatter limit
+
+import requests
+import json
+
+
+class NameGraBBer:
+    def __init__(self, client_id, access_token):
+        self.client_id = client_id
+        self.access_token = access_token
+
+    def get_user_id_from_access_token(self):
+        headers = {
+            "Authorization": "Bearer " + str(self.access_token),
+            "Client-Id": str(self.client_id),  # this is the registered api app id
+        }
+
+        try:
+            response = requests.get("https://api.twitch.tv/helix/users", headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            print("Twitch API request error occured while attempting to access users: ")
+            raise SystemExit(error)
+
+        data = response.json()
+
+        return data["data"][0]["id"]
+
+    def get_chat_names_from_user_id(self, user_id):
+        headers = {
+            "Authorization": "Bearer " + str(self.access_token),
+            "Client-Id": str(self.client_id),
+        }
+
+        params = {
+            "broadcaster_id": str(user_id),  # user is the streamer
+            "moderator_id": str(user_id),
+            "first": 1000,
+        }
+
+        try:
+            response = requests.get("https://api.twitch.tv/helix/chat/chatters", params=params, headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as error:
+            print("Twitch API request error occured while attempting to access chatters: ")
+            raise SystemExit(error)
+
+        data = response.json()
+        names = []
+
+        for entry in data["data"]:
+            names.append(entry["user_name"])
+
+        return names
